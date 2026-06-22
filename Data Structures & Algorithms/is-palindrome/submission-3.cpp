886d8693910292a0e34aa7d60d4febd4393e09c8
srushti-jawahire <jawahiresrushti@gmail.com>
2026-06-22 09:38:55 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string filtered="";
+        for(char c:s){
+            if(isalnum(c)){
+                filtered+=tolower(c);
+            }
+        }
+        int i=0;
+        int j=filtered.size()-1;
+        while(i<j){
+            if(filtered[i]!=filtered[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
